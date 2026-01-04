cd558293c7b40db891cd9c3d9e570de770601ffb
Shreesha K <saishreesha2000@gmail.com>
2026-01-04 13:25:18 +0000

    date_dimension fixed

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,14 +1,19 @@
 {{ config(materialized='table') }}
-
 with cte as (
-  select
-    TRY_TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
-    DATE(TRY_TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
-    HOUR(TRY_TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
-    {{ get_daytype(STARTED_AT) }} AS Day_Type,
-    {{ get_season(STARTED_AT) }} AS Season_of_year
-  FROM {{ source('demo', 'bike') }}
-  WHERE STARTED_AT IS NOT NULL AND START_STATION_ID!='start station id'
+select
+ TRY_TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+ DATE(TRY_TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+HOUR(TRY_TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+{{ get_season('STARTED_AT') }} AS Season_of_year,
+{{ get_daytype('STARTED_AT') }} AS Day_Type
+FROM {{ source('demo', 'bike') }}
+WHERE STARTED_AT != 'starttime'
 )
-
 select * from cte
+
+
+
+
+
+
+
