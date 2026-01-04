6515e619cfb94ec0aadfab2ff0977b7f97540831
Shreesha K <saishreesha2000@gmail.com>
2026-01-04 13:26:49 +0000

    date_dimension

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -7,7 +7,7 @@ HOUR(TRY_TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
 {{ get_season('STARTED_AT') }} AS Season_of_year,
 {{ get_daytype('STARTED_AT') }} AS Day_Type
 FROM {{ source('demo', 'bike') }}
-WHERE STARTED_AT != 'starttime'
+WHERE STARTED_AT != 'starttime' and STARTED_AT is not null
 )
 select * from cte
 
